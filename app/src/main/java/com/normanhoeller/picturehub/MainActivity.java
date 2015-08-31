a7db858208eefc4e24d4cdfecb9bf8aa72567639
Norman Hoeller <norman.hoeller@beeone.at>
2015-08-31 15:10:26 +0200

    first commit

---
+++ app/src/main/java/com/normanhoeller/picturehub/MainActivity.java
@@ -0,0 +1,61 @@
+package com.normanhoeller.picturehub;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final EditText query = (EditText) findViewById(R.id.et_query);
+        Button goSearch = (Button) findViewById(R.id.btn_go);
+
+        goSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String queryText = query.getText().toString();
+                if (!TextUtils.isEmpty(queryText)) {
+                    startSearchActivity(queryText);
+                }
+            }
+        });
+
+    }
+
+    private void startSearchActivity(String searchQuery) {
+        Intent startActivity = new Intent(this, PictureActivity.class);
+        startActivity.putExtra()
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/normanhoeller/picturehub/PictureActivity.java
@@ -0,0 +1,18 @@
+package com.normanhoeller.picturehub;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by norman on 31/08/15.
+ */
+public class PictureActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+}
