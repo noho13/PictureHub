6824a01135604c98aa22b46f84be1dc143028763
norman <norman.hoeller@gmail.com>
2015-09-02 19:46:20 +0200

    add CoordinatorLayout

--- app/src/main/java/com/normanhoeller/picturehub/ui/PictureFragment.java
+++ app/src/main/java/com/normanhoeller/picturehub/ui/PictureFragment.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.util.Log;
@@ -12,6 +11,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.normanhoeller.picturehub.PictureActivity;
 import com.normanhoeller.picturehub.R;
@@ -27,6 +27,7 @@ public class PictureFragment extends Fragment {
     private static final String PROGRESS_BAR_VISIBILITY = "progress_bar_visibility";
     private RecyclerView recyclerView;
     private ProgressBar progressBar;
+    private TextView toolbarTitle;
 
     public static PictureFragment createInstance(String searchQuery) {
         PictureFragment fragment = new PictureFragment();
@@ -43,6 +44,7 @@ public class PictureFragment extends Fragment {
         recyclerView = (RecyclerView) root.findViewById(R.id.rv_item_grid);
         progressBar = (ProgressBar) root.findViewById(R.id.pr_loading);
         progressBar.getIndeterminateDrawable().setColorFilter(getResources().getColor(android.R.color.holo_orange_light), android.graphics.PorterDuff.Mode.MULTIPLY);
+        toolbarTitle = (TextView) root.findViewById(R.id.tv_toolbar_title);
         return root;
     }
 
@@ -63,8 +65,7 @@ public class PictureFragment extends Fragment {
 
         recyclerView.setLayoutManager(new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL));
         String searchQuery = getArguments().getString(PictureActivity.SEARCH_QUERY);
-        ((AppCompatActivity) getActivity()).getSupportActionBar()
-                .setTitle("looking for: " + searchQuery.toLowerCase());
+        toolbarTitle.setText("looking for: " + searchQuery.toLowerCase());
         queryShutterStock(searchQuery);
 
     }
@@ -91,13 +92,11 @@ public class PictureFragment extends Fragment {
     }
 
     public void setResult(SearchResult searchResult) {
-        Log.d(TAG, "got searchresult: " + searchResult);
-        recyclerView.setAdapter(new PictureAdapter(searchResult.getData()));
         recyclerView.setVisibility(View.VISIBLE);
         progressBar.setVisibility(View.GONE);
+        recyclerView.setAdapter(new PictureAdapter(searchResult.getData()));
         if (searchResult.getData().size() == 0) {
             showSnackBar(recyclerView);
-//                            Toast.makeText(getActivity(), getString(R.string.no_results), Toast.LENGTH_SHORT).show();
         }
     }
 }
