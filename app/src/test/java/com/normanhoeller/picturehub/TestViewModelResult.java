45ab1279d3b13d458368913a9f114945753dcb1e
norman <norman.hoeller@gmail.com>
2016-09-12 18:34:43 +0200

    add local unit test

---
+++ app/src/test/java/com/normanhoeller/picturehub/TestViewModelResult.java
@@ -0,0 +1,29 @@
+package com.normanhoeller.picturehub;
+
+import com.normanhoeller.picturehub.model.ViewModelResult;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNotNull;
+
+/**
+ * Created by norman on 12/09/16.
+ */
+public class TestViewModelResult {
+
+    ViewModelResult result;
+
+    @Before
+    public void setup() {
+         result = new ViewModelResult("someUrl", "someDescription");
+    }
+
+    @Test
+    public void testInitialization() {
+        assertNotNull(result);
+        assertEquals("someUrl", result.getUrl());
+        assertEquals("someDescription", result.getDescription());
+    }
+}
